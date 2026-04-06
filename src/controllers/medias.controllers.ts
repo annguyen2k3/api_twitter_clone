59229444f12e6347655d437462f28528472c547f
34anguyen <annguyen300243@gmail.com>
2026-04-06 23:58:21 +0700

    feat: streaming video

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,8 +1,11 @@
 import { NextFunction, Request, Response } from 'express'
 import path from 'path'
 import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from '~/constants/dir'
+import { HTTP_STATUS } from '~/constants/httpStatus'
 import { USER_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
+import fs from 'fs'
+import mime from 'mime'
 
 // POST: /medias/upload-images
 export const uploadImageController = async (req: Request, res: Response, next: NextFunction) => {
@@ -34,4 +37,50 @@ export const serveVideoController = async (req: Request, res: Response, next: Ne
       res.status((err as any).status).send('Not found')
     }
   })
+}
+
+// GET: /static/video-stream/:filename
+export const serveVideoStreamController = (req: Request, res: Response, next: NextFunction) => {
+  const range = req.headers.range
+
+  if (!range) {
+    return res.status(HTTP_STATUS.BAD_REQUEST).send('Requires Range header')
+  }
+
+  const name = req.params.filename as string
+
+  const videoPath = path.resolve(UPLOAD_VIDEO_DIR, name)
+
+  // 1MB = 10^6 bytes
+  // Nếu tính theo hệ nhị phân thì 1MB = 2^20 bytes (1024 * 1024)
+
+  // Dung lượng video (bytes)
+  const videoSize = fs.statSync(videoPath).size
+
+  // Dung lượng video cho mỗi phần đoạn stream
+  const chunkSize = 10 ** 6 // 1MB
+
+  // Lấy giá trị byte bắt đầu từ header Range (vd: bytes=1048576-)
+  const start = Number(range.replace(/\D/g, ''))
+
+  // Lấy giá trị byte kết thúc
+  const end = Math.min(start + chunkSize, videoSize - 1)
+
+  // Dung lượng thực tế cho mỗi đoạn video stream
+  const contentLength = end - start + 1
+
+  const contentType = mime.getType(videoPath) || 'video/*'
+
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': contentLength,
+    'Content-Type': contentType
+  }
+
+  res.writeHead(HTTP_STATUS.PARTIAL_CONTENT, headers)
+
+  const videoStream = fs.createReadStream(videoPath, { start, end })
+
+  videoStream.pipe(res)
 }
--- src/routes/static.routes.ts
+++ src/routes/static.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
-import { serveImageController, serveVideoController } from '~/controllers/medias.controllers'
+import {
+  serveImageController,
+  serveVideoController,
+  serveVideoStreamController
+} from '~/controllers/medias.controllers'
 import { wrapRequestHandler } from '~/utils/handlers'
 
 const staticRouter = Router()
 
 staticRouter.get('/image/:filename', wrapRequestHandler(serveImageController))
 staticRouter.get('/video/:filename', wrapRequestHandler(serveVideoController))
+staticRouter.get('/video-stream/:filename', wrapRequestHandler(serveVideoStreamController))
 
 export default staticRouter
