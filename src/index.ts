7a22272ad801e13ad38aae189052caf74e307b99
34anguyen <annguyen300243@gmail.com>
2026-04-17 11:22:46 +0700

    feat: fake data with faker

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import { defaultErrorHandler } from './middlewares/errors.middlewares'
 import { initFolder } from './utils/file'
 import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from './constants/dir'
 import cors from 'cors'
-import { TweetType } from './constants/enums'
 
 dotenv.config()
 
